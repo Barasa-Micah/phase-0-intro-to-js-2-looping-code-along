d69127795f7bec86e750abff505bf1e1f41bc099
Barasa-Micah <barasamica@gmail.com>
2023-06-09 17:44:55 +0300

    Completed assignments

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Code your solutions in this file
+const names = ["Ever","Gabriel"];
+ const messages = writeCards(names);
+function writeCards(names) {
+    const thankYouMessages = [];
+  
+    for (let i = 0; i < names.length; i++) {
+      thankYouMessages.push(`Thank you, ${names[i]}, for the wonderful surprise gift!`);
+    }
+  
+    return thankYouMessages;
+  }
+
+  function countDown(number1){
+    for (let i = number1; i>=0; i--){
+        console.log(i);
+    }
+  }
+
+  countDown(10);
